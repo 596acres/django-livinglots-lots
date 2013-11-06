6997b63f812a2557ec29abf020ac4475f368c60e
Eric Brelsford <ebrelsford@gmail.com>
2013-11-06 11:43:10 -0500

    No more django.conf.urls.defaults

--- livinglots_lots/admin.py
+++ livinglots_lots/admin.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 from django.contrib import admin
 from django.contrib.gis.admin import OSMGeoAdmin
 from django.core.urlresolvers import reverse
