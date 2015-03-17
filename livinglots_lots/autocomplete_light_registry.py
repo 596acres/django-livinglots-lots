c750ec86614f10d7658c6ba64550bc9028086a0a
Eric Brelsford <ebrelsford@gmail.com>
2015-03-16 23:44:57 -0400

    LotAutocomplete

--- livinglots_lots/autocomplete_light_registry.py
+++ livinglots_lots/autocomplete_light_registry.py
@@ -1,6 +1,18 @@
-from autocomplete_light import AutocompleteModelBase, register
+from autocomplete_light import (AutocompleteGenericBase, AutocompleteModelBase,
+                                register)
 
-from livinglots import get_lotgroup_model
+from livinglots import get_lot_model, get_lotgroup_model
+
+
+class LotAutocomplete(AutocompleteModelBase):
+    autocomplete_js_attributes = {'placeholder': 'lot name',}
+    search_fields = ('bbl', 'name',)
+
+    def choices_for_request(self):
+        choices = super(LotAutocomplete, self).choices_for_request()
+        if not self.request.user.is_staff:
+            choices = choices.none()
+        return choices
 
 
 class LotGroupAutocomplete(AutocompleteModelBase):
@@ -14,4 +26,16 @@ class LotGroupAutocomplete(AutocompleteModelBase):
         return choices
 
 
+class AutocompleteOrganizableItems(AutocompleteGenericBase):
+    choices = (
+        get_lot_model().objects.all(),
+    )
+
+    search_fields = (
+        ('bbl', 'name',),
+    )
+
+
+register(AutocompleteOrganizableItems)
+register(get_lot_model(), LotAutocomplete)
 register(get_lotgroup_model(), LotGroupAutocomplete)
